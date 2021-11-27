5a0960d1a4896808898aca8e1a85a88a2b8aa8d5
Kai-En Lin <kaienlin1999@gmail.com>
2021-11-27 23:51:47 +0800

    Define the attributes of an intersection with proper getters and setters

---
+++ discrete_intersection_simulator/intersection.py
@@ -0,0 +1,87 @@
+from typing import Tuple, Set, Dict
+import copy
+
+class Intersection:
+    def __init__(self) -> None:
+        self.__conflict_zones: Set[str] = set()
+        self.__transitions: Set[Tuple[str, str]] = set()
+        self.__adjacency_list: Dict[str, Set[str]] = dict()
+        self.__trajectories: Set[Tuple[str]] = set()
+        self.__src_lanes: Set[str] = set()
+        self.__dst_lanes: Set[str] = set()
+        self.__cz_coordinates: Dict[str, Tuple[float, float]] = dict()
+
+    def add_conflict_zone(self, cz_id: str) -> None:
+        '''
+        Add a conflict zone to the intersection.
+        '''
+        self.__conflict_zones.add(cz_id)
+        self.__adjacency_list[cz_id] = set()
+
+    def add_transition(self, from_cz_id: str, to_cz_id: str) -> None:
+        '''
+        Add a transition to the intersection.
+        A transition is an ordered pair of CZs representing a valid move between the two CZs.
+        '''
+        if from_cz_id not in self.__conflict_zones:
+            raise Exception(f"[Intersection.add_edge] vertex {from_cz_id} does not exist.")
+        if to_cz_id not in self.__conflict_zones:
+            raise Exception(f"[Intersection.add_edge] vertex {to_cz_id} does not exist.")
+        self.__transitions.add((from_cz_id, to_cz_id))
+        self.__adjacency_list[from_cz_id].add(to_cz_id)
+
+    def add_trajectory(self, trajectory: Tuple[str]) -> None:
+        '''
+        Add a trajectory to the intersection.
+        If an adjacent pair in the trajectory is not in the set of edges, add it.
+        '''
+        self.__trajectories.add(trajectory)
+        for i in range(len(trajectory) - 1):
+            if (trajectory[i], trajectory[i+1]) not in self.__transitions:
+                self.__transitions.add((trajectory[i], trajectory[i+1]))
+
+    def add_src_lane(self, src_lane_id: str) -> None:
+        '''
+        Add a source lane identifier to the intersection.
+        '''
+        self.__src_lanes.add(src_lane_id)
+
+    def add_dst_lane(self, dst_lane_id: str) -> None:
+        '''
+        Add a destination lane identifier to the intersection.
+        '''
+        self.__dst_lanes.add(dst_lane_id)
+
+    def set_cz_coordinate(self, cz_id: str, x: float, y: float) -> None:
+        '''
+        Set the coordinate of a conflict zone.
+        This information may be used in rendering of state.
+        '''
+        if cz_id not in self.__conflict_zones:
+            raise Exception(f"[Intersection.set_cz_coordinate] cz_id {cz_id} not in conflict zones")
+        self.__cz_coordinates[cz_id] = (x, y)
+
+    @property
+    def conflict_zones(self) -> Set[str]:
+        return copy.deepcopy(self.__conflict_zones)
+
+    @property
+    def transitions(self) -> Set[Tuple[str, str]]:
+        return copy.deepcopy(self.transitions)
+
+    @property
+    def adjacency_list(self) -> Dict[str, Set[str]]:
+        return copy.deepcopy(self.__adjacency_list)
+
+    @property
+    def trajectories(self) -> Set[Tuple[str]]:
+        return copy.deepcopy(self.__trajectories)
+
+    @property
+    def src_lanes(self) -> Set[str]:
+        return copy.deepcopy(self.__src_lanes)
+
+    @property
+    def dst_lanes(self) -> Set[str]:
+        return copy.deepcopy(self.__dst_lanes)
+
