0cac5d30633713f90b40ab07919d92225ebe2fc4
Kai-En Lin <kaienlin1999@gmail.com>
2021-12-27 13:44:37 +0800

    Refactor DP.py

--- src/DP.py
+++ src/DP.py
@@ -1,13 +1,11 @@
-import math
-import sys, pprint, random
+import random
 import numpy as np
 from tqdm import tqdm
 
 from environment import ProbabilisticEnv
-from simulator import intersection
-from utility import get_4cz_intersection
+from utility import read_intersection_from_json
 
-def value_iteration(env: ProbabilisticEnv, theta=1e-3, discount_factor=0.9):
+def value_iteration(env: ProbabilisticEnv, theta=1e-3, discount_factor=0.95):
     def one_step_lookahead(state, V):
         A = np.zeros(env.action_space_size)
         for a in range(env.action_space_size):
@@ -21,23 +19,26 @@ def value_iteration(env: ProbabilisticEnv, theta=1e-3, discount_factor=0.9):
     while delta > theta:
         epoch += 1
         delta = 0.0
-        for s in tqdm(range(env.state_space_size), leave=False, ascii=True):
+        for s in tqdm(range(env.state_space_size), ncols=70, leave=False, ascii=True):
             A = one_step_lookahead(s, V)
             best_action_value = np.min(A)
             delta = max(delta, np.abs(V[s] - best_action_value))
             V[s] = best_action_value
         print(f"epoch {epoch}: delta = {delta}")
 
-    policy = np.zeros((env.state_space_size, env.action_space_size))
+    Q = np.zeros((env.state_space_size, env.action_space_size))
     for s in range(env.state_space_size):
         A = one_step_lookahead(s, V)
-        best_action = np.argmin(A)
-        policy[s, best_action] = 1.0
+        Q[s] = A
 
-    return policy
-
+    return Q
 
 if __name__ == "__main__":
-    intersection = get_4cz_intersection()
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    intersection = read_intersection_from_json("../intersection_configs/1x1.json")
     env = ProbabilisticEnv(intersection)
-    value_iteration(env)
+    DP_policy = value_iteration(env)
+
+    np.save("./DP.npy", DP_policy)
