b3bcd6e0e26f5f1c493da163e6bcfe16da86165c
Kai-En Lin <kaienlin1999@gmail.com>
2021-12-30 23:34:22 +0800

    Add detection of infinite waiting in policy.QTablePolicy

--- src/policy/value_function.py
+++ src/policy/value_function.py
@@ -5,6 +5,28 @@ from .base import Policy
 class QTablePolicy(Policy):
     def __init__(self, Q):
         self.Q = Q
+        
+        self.prev_state: int = -1
+        self.prev_action: int = -1
+        self.waiting_counter: int = 0
+        self.max_waiting = 100
 
     def decide(self, state: int) -> int:
-        return np.argmin(self.Q[state])
+        Q_state = self.Q[state]
+        action = np.argmin(Q_state)
+        if action == 0:
+            if state == self.prev_state:
+                self.waiting_counter += 1
+            else:
+                self.waiting_counter = 1
+        else:
+            self.waiting_counter = 0
+
+        if self.waiting_counter > self.max_waiting:
+            print(f"on state {state} max waiting time exceeded")
+            Q_state[action] = np.inf
+            action = np.argmin(Q_state)
+
+        self.prev_state = state
+        self.prev_action = action
+        return action
