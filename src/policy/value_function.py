c6401b49510024c06d33e2e10dab10a50076d4c5
Kai-En Lin <kaienlin1999@gmail.com>
2022-03-09 15:07:15 +0800

    Block NO-OP and actions that directly cause deadlocks in QTablePolicy

--- src/policy/value_function.py
+++ src/policy/value_function.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .base import Policy
 from .greedy import IGreedyPolicy
+from utility import Digraph
 
 class QTablePolicy(Policy):
     def __init__(self, env, Q):
@@ -21,7 +22,40 @@ class QTablePolicy(Policy):
             return self.igreedy.decide(state)
 
         Q_state = self.Q[state]
-        action = np.argmin(Q_state)
+        effective_actions = [a for a in range(self.env.action_space_size) if self.env.is_effective_action_of_state(a, state)]
+
+        G = Digraph()
+        decoded_state = self.env.decode_state(state)
+        for vehicle_state in decoded_state:
+            if 0 <= vehicle_state.position <= len(vehicle_state.trajectory) - 2:
+                cur_cz = vehicle_state.trajectory[vehicle_state.position]
+                next_cz = vehicle_state.trajectory[vehicle_state.position + 1]
+                G.add_edge(cur_cz, next_cz)
+
+        for action in list(effective_actions):
+            if action == 0:
+                continue
+            vehicle_state = decoded_state[action - 1]
+            if vehicle_state.position >= len(vehicle_state.trajectory) - 2:
+                continue
+            cz1 = vehicle_state.trajectory[vehicle_state.position + 1]
+            cz2 = vehicle_state.trajectory[vehicle_state.position + 2]
+            G.add_edge(cz1, cz2)
+            if vehicle_state.position > -1:
+                G.remove_edge(vehicle_state.trajectory[vehicle_state.position], cz1)
+            cyclic = G.has_cycle()
+            G.remove_edge(cz1, cz2)
+            if vehicle_state.position > -1:
+                G.add_edge(vehicle_state.trajectory[vehicle_state.position], cz1)
+            if cyclic:
+                effective_actions.remove(action)
+
+        # block NO-OP action
+        if 0 in effective_actions and len(effective_actions) > 1 and self.igreedy.decide(state) != 0:
+            effective_actions.remove(0)
+
+        action = effective_actions[Q_state[effective_actions].argmin()]
+
         if action == 0:
             if state == self.prev_state:
                 self.waiting_counter += 1
