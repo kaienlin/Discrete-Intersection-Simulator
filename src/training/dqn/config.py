b2d81a9d336c0c7f7505cfa707b67702434b2575
Jason Huang <55070744+JasonHuang2000@users.noreply.github.com>
2022-03-30 16:12:07 +0800

    DDQN agent implementation and extra parameters configuration (#10)
    * Add checkpoint saving mechanism in DQNTrainer
    
    * Add DDQN agent implementation
    
    * Remove redundant code
    
    Co-authored-by: Shao-Ching Huang <b08902049@csie.ntu.edu.tw>

--- src/training/dqn/config.py
+++ src/training/dqn/config.py
@@ -6,7 +6,8 @@ class config:
 
     # networks
     # refer to https://www.tensorflow.org/agents/api_docs/python/tf_agents/networks/q_network/QNetwork
-    fc_layer_params = (75, 40)
+    use_ddqn = True
+    fc_layer_params = (150, 150)
     dropout_layer_params = (0.1, 0.1)
     conv_layer_params = None
     activation_fn = tf.keras.activations.relu
@@ -19,9 +20,13 @@ class config:
     lr = 1e-3
     betas = (0.9, 0.999)
     eps = 1e-6
+    gamma = 0.99
+    target_update_tau = 1.0
+    target_update_period = 100
     td_errors_loss_fn = common.element_wise_squared_loss
 
     # data collection
+    initial_collect_epispode = 100
     replay_buffer_max_length = 20000
     collect_steps_per_epoch = 10
 
