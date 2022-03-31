3ccc0391fec567e331e1db5f8e639b5d0db467cc
Jason Huang <55070744+JasonHuang2000@users.noreply.github.com>
2022-03-31 09:14:43 +0800

    Batched training implementation (#11)
    * Add checkpoint saving mechanism in DQNTrainer
    
    * Add DDQN agent implementation
    
    * Remove redundant code
    
    * Implement batched training
    
    Co-authored-by: Shao-Ching Huang <b08902049@csie.ntu.edu.tw>

--- src/training/dqn/config.py
+++ src/training/dqn/config.py
@@ -7,7 +7,7 @@ class config:
     # networks
     # refer to https://www.tensorflow.org/agents/api_docs/python/tf_agents/networks/q_network/QNetwork
     use_ddqn = True
-    fc_layer_params = (150, 150)
+    fc_layer_params = (100, 50)
     dropout_layer_params = (0.1, 0.1)
     conv_layer_params = None
     activation_fn = tf.keras.activations.relu
@@ -16,25 +16,26 @@ class config:
     # training
     is_cuda = True
     num_iterations = 100000
-    batch_size = 1
+    batch_size = 64
     lr = 1e-3
     betas = (0.9, 0.999)
     eps = 1e-6
     gamma = 0.99
+    epsilon_greedy = 0.1
     target_update_tau = 1.0
     target_update_period = 100
     td_errors_loss_fn = common.element_wise_squared_loss
 
     # data collection
-    initial_collect_epispode = 100
-    replay_buffer_max_length = 20000
-    collect_steps_per_epoch = 10
+    initial_collect_steps = 1000
+    replay_buffer_max_length = 100000
+    collect_steps_per_iteration = 10
 
     # loggings
     log_iterval = 200
-    ckpt_interval = 500
+    ckpt_interval = 2000
     ckpt_kept_num = 5
 
     # validation
     valid_data_dir = "../testdata/test-2x2-s/"
-    valid_interval = 500
+    valid_interval = 1000
