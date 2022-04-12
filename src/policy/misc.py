983934d2acf61b1a8b3a8eb6415407f33f88697f
Kai-En Lin <kaienlin1999@gmail.com>
2022-04-12 20:50:24 +0800

    Change RGS implementation

--- src/policy/misc.py
+++ src/policy/misc.py
@@ -1,4 +1,5 @@
 from environment.tabular.vehicle_based import VehicleBasedStateEnv
+from utility import Digraph
 
 from .base import Policy
 
@@ -10,13 +11,31 @@ class SingleCzPolicy(Policy):
 
     def decide(self, state: int) -> int:
         decoded_state = self.env.decode_state(state)
-
+        G = Digraph()
+        for vehicle_state in decoded_state:
+            if 0 <= vehicle_state.position <= len(vehicle_state.trajectory) - 2:
+                cur_cz = vehicle_state.trajectory[vehicle_state.position]
+                next_cz = vehicle_state.trajectory[vehicle_state.position + 1]
+                G.add_edge(cur_cz, next_cz)
+        
         available = True
         for i, vehicle_state in enumerate(decoded_state):
             if vehicle_state.position == 0:
-                if vehicle_state.state == "waiting":
-                    return i + 1
                 available = False
+            if vehicle_state.position == 0 and vehicle_state.state == "waiting":
+                if vehicle_state.position >= len(vehicle_state.trajectory) - 2:
+                    return i + 1
+                cz1 = vehicle_state.trajectory[vehicle_state.position + 1]
+                cz2 = vehicle_state.trajectory[vehicle_state.position + 2]
+                G.add_edge(cz1, cz2)
+                if vehicle_state.position > -1:
+                    G.remove_edge(vehicle_state.trajectory[vehicle_state.position], cz1)
+                cyclic = G.has_cycle()
+                G.remove_edge(cz1, cz2)
+                if vehicle_state.position > -1:
+                    G.add_edge(vehicle_state.trajectory[vehicle_state.position], cz1)
+                if not cyclic:
+                    return i + 1
         
         if not available:
             return 0
@@ -37,6 +56,10 @@ class RGS(Policy):
         self.dependency_cycles = [
             {("1", "3"), ("3", "4"), ("4", "2"), ("2", "1")}
         ]
+        if len(env.sim.intersection.conflict_zones) == 2:
+            self.dependency_cycles = [
+                {("1", "2"), ("2", "1")}
+            ]
 
     def decide(self, state: int) -> int:
         decoded_state = self.env.decode_state(state)
@@ -48,37 +71,45 @@ class RGS(Policy):
                     (vehicle_state.trajectory[vehicle_state.position], vehicle_state.trajectory[vehicle_state.position + 1])
                 )
 
-        for tau in range(self.tau, 0, -1):
-            ok = True
-            for C in self.dependency_cycles:
-                if len(transitions.intersection(C)) > len(C) - tau:
-                    ok = False
-                    break
-            if not ok:
-                continue
-
+        ok = True
+        for C in self.dependency_cycles:
+            if len(transitions.intersection(C)) > len(C) - self.tau:
+                ok = False
+                break
+        
+        if not ok:
             for i, vehicle_state in enumerate(decoded_state):
-                if vehicle_state.state == "waiting":
-                    if vehicle_state.position >= len(vehicle_state.trajectory) - 2:
+                if vehicle_state.state == "waiting" and vehicle_state.position >= 0:
+                    if vehicle_state.position == len(vehicle_state.trajectory) - 1:
                         return i + 1
+                    cur_cz = vehicle_state.trajectory[vehicle_state.position]
                     next_cz = vehicle_state.trajectory[vehicle_state.position + 1]
-                    next2_cz = vehicle_state.trajectory[vehicle_state.position + 2]
-
-                    if vehicle_state.position >= 0:
-                        cur_cz = vehicle_state.trajectory[vehicle_state.position]
-                        transitions.remove((cur_cz, next_cz))
-
-                    transitions.add((next_cz, next2_cz))
-                    ok = True
-                    for C in self.dependency_cycles:
-                        if len(transitions.intersection(C)) > len(C) - tau:
-                            ok = False
-                    if ok:
+                    if any((cur_cz, next_cz) in C for C in self.dependency_cycles):
                         return i + 1
-                    
-                    transitions.remove((next_cz, next2_cz))
-                    if vehicle_state.position >= 0:
-                        cur_cz = vehicle_state.trajectory[vehicle_state.position]
-                        transitions.add((cur_cz, next_cz))
             return 0
+
+        for i, vehicle_state in enumerate(decoded_state):
+            if vehicle_state.state == "waiting":
+                if vehicle_state.position >= len(vehicle_state.trajectory) - 2:
+                    return i + 1
+                next_cz = vehicle_state.trajectory[vehicle_state.position + 1]
+                next2_cz = vehicle_state.trajectory[vehicle_state.position + 2]
+
+                if vehicle_state.position >= 0:
+                    cur_cz = vehicle_state.trajectory[vehicle_state.position]
+                    transitions.remove((cur_cz, next_cz))
+
+                transitions.add((next_cz, next2_cz))
+                ok = True
+                for C in self.dependency_cycles:
+                    if len(transitions.intersection(C)) > len(C) - self.tau:
+                        ok = False
+                if ok:
+                    return i + 1
+                
+                transitions.remove((next_cz, next2_cz))
+                if vehicle_state.position >= 0:
+                    cur_cz = vehicle_state.trajectory[vehicle_state.position]
+                    transitions.add((cur_cz, next_cz))
+
         return 0
